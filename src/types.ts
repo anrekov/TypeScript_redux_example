5e4afb52c78d84cd3e995b93d425083030ddc73a
anrekov <arekov.exceedteam@gmail.com>
2021-03-12 13:27:43 +0300

    Redux with TS

--- src/types.ts
+++ src/types.ts
@@ -1,40 +1,48 @@
-import { ADD_TASK, REMOVE_TASK, COMPLETE_TASK, CHANGE_FILTER } from './constants';
+import {
+  ADD_TASK,
+  REMOVE_TASK,
+  COMPLETE_TASK,
+  CHANGE_FILTER,
+} from './constants';
 
 // Store
 export type Filter = string;
 
 export interface ITask {
-    id: number,
-    text: string,
-    isCompleted: boolean,
+  id: number;
+  text: string;
+  isCompleted: boolean;
 }
 
 // Actions
 interface IAddTaskAction {
-    type: typeof ADD_TASK,
-    payload: ITask,
+  type: typeof ADD_TASK;
+  payload: ITask;
 }
 
 interface IRemoveTaskAction {
-    type: typeof REMOVE_TASK,
-    payload: {
-        id: number,
-    }
+  type: typeof REMOVE_TASK;
+  payload: {
+    id: number;
+  };
 }
 
 interface ICompleteTaskAction {
-    type: typeof COMPLETE_TASK,
-    payload: {
-        id: number,
-    }
+  type: typeof COMPLETE_TASK;
+  payload: {
+    id: number;
+  };
 }
 
 interface IChangeFilterAction {
-    type: typeof CHANGE_FILTER,
-    payload: {
-        activeFilter: Filter,
-    }
+  type: typeof CHANGE_FILTER;
+  payload: {
+    activeFilter: Filter;
+  };
 }
 
-export type TaskActionTypes = IAddTaskAction | IRemoveTaskAction | ICompleteTaskAction;
+export type TaskActionTypes =
+  | IAddTaskAction
+  | IRemoveTaskAction
+  | ICompleteTaskAction;
 export type FilterActionType = IChangeFilterAction;
